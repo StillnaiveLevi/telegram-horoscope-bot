d1b7449784ecfd61eca5579bf59921f40837c559
StillnaiveLevi <merkebbirhane3@gmail.com>
2026-06-13 18:37:24 +0300

    add simple implementation of the bot

--- bot.py
+++ bot.py
@@ -20,3 +20,23 @@ def get_daily_horscope(sign: str, day: str)-> dict:
     response = requests.get(url, params)
 
     return response.json()
+
+@bot.message_handler(commands=['horoscope'])
+def sign_handler(message):
+    text:"What's your zodiac sign?"
+    sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
+    bot.register_next_step_handler(sent_msg, day_handler)
+
+def day_handler(message):
+    sign = message.text
+    text = "what day you want to know?\nChoose one: *TODAY*, *TOMORROW*, *YESTERDAY*, or a date in format YYYY-MM-DD."
+    sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
+    bot.register_next_step_handler(sent_msg, fetch_horoscope, sign.capitalize())
+
+def fetch_horoscope(message, sign):
+    day = message.text
+    horoscope = get_daily_horoscope(sign, day)
+    data = horoscope["data"]
+    horoscope_message = f'*Horoscope:* {data["horoscope_data"]}\\n*Sign:* {sign}\\n*Day:* {data["date"]}'
+    bot.send_message(message.chat.id, "Here's your horoscope!")
+    bot.send_message(message.chat.id, horoscope_message, parse_mode="Markdown")
